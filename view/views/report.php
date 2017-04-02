760daa8fdf85f9c2bc4768caa9ae2e3853c97a30
andymartinj <andy-greenday@hotmail.com>
2017-04-02 17:43:46 +0200

    kmom01 done

--- view/views/report.php
+++ view/views/report.php
@@ -3,7 +3,87 @@
             <h1>Redovisningar</h1>
             <p>Här ligger alla redoviningar för kusen oophp.</p>
             <h2>Kmom01</h2>
-            <p>Text kommer här.</p>
+            <p>Detta blev ett ganska omfattande kursmoment med rätt många uppgiften,
+            övningar och annat material som skulle gås igenom. Resultatet för mig blev
+            att jag valde att inte koda igenom övningarna då de hade tagit för
+            mycket av min tid. Det var tillräckligt för mig att gå noga läsa igenom
+            övningarna för att friska upp minnet (php20 och oophp20). Personligen
+            tycker jag det blev för mycket i detta första kursmoment, och som jag ser
+            i redovisningstråden såhär på söndag eftermiddag så tycker många detsamma.
+            Det var precis jag hann med att göra uppgifterna, och då har jag suttit
+            hela helgen. Jag hade ingen tid att läsa igenom atriklarna eller annat
+            läsmaterial, utan det blev direkt på övningarna och sen uppgifterna.
+            Jag hade väldigt gärna haft lite mer tid på mig för att noga gå igenom
+            allting och ta in allt nytt - för mig tog det mer än 20 timmar att
+            göra detta kursmoment och det är inte inklusive läsning. Så min personliga
+            åsikt är att ni bör dela upp det bättre och överdriva med uppgifter i
+            första kursmomentet där man också måste installera labbmiljö för båda kurserna.
+            <br><br>
+            Guessuppgiften fick ingen style, då jag hade lite problem med att få
+            sessions att fungera (på grund av egen dumhet). Det var en nyttigt uppgift
+            för att friska upp minnet och komma igång med PHP igen. Jag gjorde inget
+            speciellt, bara följde strukturen som stod i uppgiften (så som jag har
+            tolkat det i alla fall). Det är inget mästerverk men det fungerar.
+            <br><br>
+            Till Anax-lite valde jag att jobba med Makefiler och LESS som vi gjorde
+            i designkursen, då jag gillar design och vill försöka hålla igång med
+            LESS och Makefiler. Det känns nyttigt att ha koll på och det är dessutom
+            kul att göra designen helt från scratch. Även om den än så länge inte
+            är speciellt grym då jag inte hade mycket tid att lägga på det just i
+            detta kursmoment. Jag kopierade över Makefilen från designkursen och
+            installerade paketen från npm som behövs för att kopilera LESS.
+            <br><br>
+            Jag gjorde alla extrauppgifterna för Anax-lite (inte navbaren) då
+            jag faktiskt redan hade gjort dem när jag först kollade. Jag har
+            lagt till så att Cimage används för bilder. En view byline skapades
+            som används på About-sidan. En flash-view som används på alla sidor
+            under navbaren och en testsida som jag använde för att testa den nya
+            navbar1.<br><br>
+            Min navbar1 används nu på alla sidor och den är egentligen inget speciellt.</p>
+            <h4>Hur känns det att hoppa rakt in i klasser med PHP, gick det bra?</h4>
+            <p>Kände lite "omg hur tusan kodar man med PHP" i början men det
+            kändes bra sen. När vi kommer direkt från oo-python så känns det
+            naturligt att jobba med klasser och objekt, så just det var inga problem
+            alls att komma igång med. Jag märkte hur lätt det blev att komma igång
+            med PHP igen efter att ha gått igenom övningarna. Kändes som att allt
+            jag behövde göra var att skumma igenom dem och sen var man inne i
+            PHP-kodning igen.</p>
+            <h4>Berätta om dina reflektioner kring ramverk, anax-lite och din me-sida.</h4>
+            <p>Tyckte det var trevligt att få göra ramverket och inte få det helt
+            färdigt. Man får bättre koll på hur det fungerar, hur det är uppdelat och
+            var man ska lägga sin kod. Jag hade dock gärna gått in och analyserat
+            filerna i ramverket lite mer och gjort egna ändraingar/förbättringar,
+            men som sagt så var det ganska ont om tid så det får bli i kommande moment.
+            Men Anax-lite verkar bra och lätt att ha med att göra, och det var
+            mycket kul att komma igång med även om det tog sin lilla tid.
+            <br><br>
+            Alla krav är implementerade, första sidan, about, report finns plus
+            testsidan och en route "/status" som jag gjorde i övningen, som returnerar
+            ett JSON-objekt. "Diverse detaljer om ditt system" är inte väldigt specifikt,
+            så jag bara antog att det är samma som i övningen.
+            <br><br>
+            Navbaren gjorde jag från en key-value array som loopas igenom och lagrar
+            html-koden i en variabel som jag använder i vyn. För att sätta en
+            active klass skickar jag bara med en variabel från routern - det var
+            inget speciellt krav på just detta så jag har tid att förbättra det i
+            senare kursmoment.<br><br>
+            Som sagt gjorde jag designen med LESS. Det är inget mästerverk men
+            jag ser det som något vi kan jobba på genom hela kursen - så det finns
+            tid för förbättringar sen. Dock är jag nöjd med den stilen jag har just
+            nu. Testa lite andra färger än vad jag brukar, ett litet annorlunda tema.
+            Det blev snyggt och enkelt tycker jag. I allmänhet är jag hittills
+            mycket nöjd med min me-sida, speciellt med tanke på tidspressen.</p>
+            <h4>Gick det bra att komma igång med MySQL, har du liknande erfarenheter sedan tidigare?</h4>
+            <p>Det gick bra, inga konstigheter. Jag gjorde till uppgift 9, men
+            vilket kanske är lite mer än en tredjedel. Kände att jag hade bra koll
+            på det och kom ihåg mycket från SQL labben vi hade i htmlphp. Jag kommer
+            ihåg mycket från den vilket gjorde det super lätt att komma igng med
+            MySQL i Workbench. Annars har jag ingen erfarenhet med MySQL.<br><br>
+            Jag valde att jobba i localhost då jag har varit iväg lite och inte
+            alltid suttit på ett stadigt nätverk, så det kändes säkrast att köra
+            i localhost. Kanske gör om det snabbt på BTHs server nästa vecka då
+            jag tycker det känns bättre när man kör mot en riktig server. Man känner
+            sig lite coolare då.</p>
             <h2>Kmom02</h2>
             <p>Text kommer här.</p>
             <h2>Kmom03</h2>
